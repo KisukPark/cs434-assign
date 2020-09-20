489d586863594380ba4374ed8fea5a262c8397cd
kisukpark <kisukpark@MacBook-Pro-2.T-mobile.com>
2020-09-20 22:29:13 +0900

    Modify implementations to use smaller functions

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -26,24 +26,24 @@ object FunSets {
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-  def union(s: Set, t: Set): Set = (i: Int) => s(i) || t(i)
+  def union(s: Set, t: Set): Set = (i: Int) => contains(s, i) || contains(t, i)
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-  def intersect(s: Set, t: Set): Set = (i: Int) => s(i) && t(i)
+  def intersect(s: Set, t: Set): Set = (i: Int) => contains(s, i) && contains(t, i)
 
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-  def diff(s: Set, t: Set): Set = (i: Int) => s(i) && !t(i)
+  def diff(s: Set, t: Set): Set = (i: Int) => contains(s, i) && !contains(t, i)
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-  def filter(s: Set, p: Int => Boolean): Set = (i: Int) => s(i) && p(i)
+  def filter(s: Set, p: Int => Boolean): Set = (i: Int) => contains(s, i) && contains(p, i)
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
